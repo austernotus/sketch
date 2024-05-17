3094059ca5b0ccb2740fefd92fcd1b1b17c30adf
austernotus <76696499+austernotus@users.noreply.github.com>
2024-05-17 15:43:21 +0100

    size button logic added

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container")
+const sizeButton = document.getElementById("sizeButton")
+
 
 for(i=0;i<256;i++){
     let newDiv = document.createElement("div")
@@ -6,5 +8,24 @@ for(i=0;i<256;i++){
     newDiv.addEventListener("mouseover", () => newDiv.style.backgroundColor = "white")
     container.appendChild(newDiv);
 }
+
+function askSize(){
+
+    let newSize = Number(prompt("Pick a size. (max: 100px)"))
+
+    console.log(newSize)
+    if (!isNaN(newSize) && Number.isInteger(newSize) && newSize <= 100 && newSize > 0){
+
+    }
+    else
+    {
+        console.log("not a valid number")
+        askSize()
+    }
+}
+
+
+
+sizeButton.addEventListener("click", () => askSize())
 
 console.log(container)
